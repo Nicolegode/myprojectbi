7d67c640fc45d74109298e0b1df33aaa1742574d
Nicole Gode <nicolegodevg@hotmail.com>
2024-05-24 11:28:15 -0300

    Atualizacao

--- Stage/dbo.controle_carga.sql
+++ Stage/dbo.controle_carga.sql
@@ -1,5 +1,15 @@
-CREATE TABLE dbo.controle_carga;
-
+IF NOT EXISTS(
+    SELECT *
+    FROM sys.tables as t
+    JOIN sys.schemas AS s ON t.schema_id = s.schema_id
+    WHERE t.name = 'controle_carga'
+        AND s.name = 'dbo')
+      
+CREATE TABLE dbo.controle_carga
+( 
+    nome_tabela varchar(1000),
+    ultima_chave_inserida int
+)
 INSERT INTO dbo.controle_carga (nome_tabela, ultima_chave_inserida)
 VALUES('employees', NULL),
       ('Customers',NULL),
